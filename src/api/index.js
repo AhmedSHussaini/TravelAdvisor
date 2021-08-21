1e3c3f5ebb4a4c1a431abc56c76e0ebe607ca460
Ahmed Hussaini <ahmedhussaini02@yahoo.ca>
2021-08-21 11:42:16 -0400

    v1

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+export const getPlacesData = async (type, sw, ne) => {
+    try{
+        const { data: { data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+            },
+            headers: { 
+                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                'x-rapidapi-key': '25669b6b0emshbfc72f6a846d221p122bd0jsn6f2c465d0036'
+            }
+        });
+
+        return data;
+    } catch(error){
+        console.log(error);
+    }
+}
